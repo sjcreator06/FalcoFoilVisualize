5a5539ac0b0df4f145617b1c3073978d09e7152a
Samuel Joseph <73075829+sjcreator06@users.noreply.github.com>
2025-06-07 18:13:20 -0700

    Update foilTransformations.py

--- Foil_Analysis/foilTransformations.py
+++ Foil_Analysis/foilTransformations.py
@@ -15,156 +15,216 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 from scipy.interpolate import CubicSpline
 
-# NACA 4-Digit Airfoil Coordinates
-i = 0
-data = []
-foilNames = []
-x = []
-y = []
-
-# Creating Data Array from CSV File
-dataFile = open("Foil Data/airfoil_data_equalized.csv", "r")
-for line in dataFile:
-    data.append(line.strip())
-
-# Creating X and Y Coordinate String Arrays
-for line in range(len(data)):
-    if i % 2 == 0:
-        x.append(data[i].split(","))
-    else:
-        y.append(data[i].split(","))
+def foilData():
+    # NACA 4-Digit Airfoil Coordinates
+    global foilNames
     
-    i += 1
+    i = 0
+    data = []
+    foilNames = []
+    x = []
+    y = []
+
+    # Creating Data Array from CSV File
+    dataFile = open("Foil_Data/airfoil_data_equalized.csv", "r")
+    for line in dataFile:
+        data.append(line.strip())
+
+    # Creating X and Y Coordinate String Arrays
+    for line in range(len(data)):
+        if i % 2 == 0:
+            x.append(data[i].split(","))
+        else:
+            y.append(data[i].split(","))
     
-# Converting String Arrays to Float Arrays
-for i in range(len(x)):
-    for j in range(len(x[i])):
-        x[i][j] = float(x[i][j])
-        y[i][j] = float(y[i][j])
-
-# Creating an Array of Airfoil Names
-foilNamesFile = open("Foil Data/NACA_4-Digit_Foil_Names.txt", "r")
-
-for foil in foilNamesFile:
-    foilNames.append(foil)
-
-
-# Airfoil Indicies (Follows the order of NACA_4-Digit_Foil_Names.txt)
-mediumFoils = [1,2,3,8,9,15,20,21]
-thickFoils = [10,11,12,22,23]
-
-# Finding Maximum Y Coordinate for Each Airfoil 
-mediumFoilsYMax = []
-thickFoilsYMax = []
-
-for foil in mediumFoils:
-    maxYCoord = max(y[foil])
-    mediumFoilsYMax.append(maxYCoord)
-
-for foil in thickFoils:
-    maxYCoord = max(y[foil])
-    thickFoilsYMax.append(maxYCoord)
-
-# Creating Dictionaries Key Value Pairs (Air Foil Index - Max Y Coordinate)
-mediumFoilsDict = dict(zip(mediumFoils, mediumFoilsYMax))
-thickFoilsDict = dict(zip(thickFoils, thickFoilsYMax))
-
-# Sorted Dictionaries by Max Y Coordinate Value (Air Foil Index - Max Y Coordinate) in Increasing Order
-mediumFoilsDictSorted = dict(sorted(mediumFoilsDict.items(), key=lambda item: item[1]))
-thickFoilsDictSorted = dict(sorted(thickFoilsDict.items(), key=lambda item: item[1]))
-
-# Airfoil Indexes in Increasing Order of Thickness
-mediumFoilIndexSorted = list(mediumFoilsDictSorted.keys())
-thickFoilIndexSorted = list(thickFoilsDictSorted.keys())
-
-# print("Medium Foils Sorted by Thickness: ", mediumFoilIndexSorted)
-# print("Thick Foils Sorted by Thickness: ", thickFoilIndexSorted)
-
-# Pre-Allocating Arrays
-x3 = [0]*len(mediumFoilIndexSorted)
-y3= [0]*len(mediumFoilIndexSorted)
-x4 = [0]*len(thickFoilIndexSorted)
-y4= [0]*len(thickFoilIndexSorted)
-
-# Sorting X and Y Arrays
-i=0
-for index in mediumFoilIndexSorted:
-    x3[i] = x[index]
-    y3[i] = y[index]
-    i+=1
-
-i=0
-for index in thickFoilIndexSorted:
-    x4[i] = x[index]
-    y4[i] = y[index]
-    i+=1
+        i += 1
     
-# Airfoil Tranformation Animation
-figure, axis = plt.subplots()
-foil, = axis.plot([], [], lw=2, color='blue')
-axis.set_xlim(-0.1, 1.1)
-axis.set_ylim(-0.25, 0.25)
+    # Converting String Arrays to Float Arrays
+    for i in range(len(x)):
+        for j in range(len(x[i])):
+            x[i][j] = float(x[i][j])
+            y[i][j] = float(y[i][j])
+
+    # Creating an Array of Airfoil Names
+    foilNamesFile = open("Foil_Data/NACA_4-Digit_Foil_Names.txt", "r")
+
+    for foil in foilNamesFile:
+        foilNames.append(foil)
 
 
+    # Airfoil Indicies (Follows the order of NACA_4-Digit_Foil_Names.txt)
+    mediumFoils = [1,2,3,8,9,15,20,21]
+    thickFoils = [10,11,12,22,23]
+
+    # Finding Maximum Y Coordinate for Each Airfoil 
+    mediumFoilsYMax = []
+    thickFoilsYMax = []
+
+    for foil in mediumFoils:
+        maxYCoord = max(y[foil])
+        mediumFoilsYMax.append(maxYCoord)
+
+    for foil in thickFoils:
+        maxYCoord = max(y[foil])
+        thickFoilsYMax.append(maxYCoord)
+
+    # Creating Dictionaries Key Value Pairs (Air Foil Index - Max Y Coordinate)
+    mediumFoilsDict = dict(zip(mediumFoils, mediumFoilsYMax))
+    thickFoilsDict = dict(zip(thickFoils, thickFoilsYMax))
+
+    # Sorted Dictionaries by Max Y Coordinate Value (Air Foil Index - Max Y Coordinate) in Increasing Order
+    
+    mediumFoilsDictSorted = dict(sorted(mediumFoilsDict.items(), key=lambda item: item[1]))
+    thickFoilsDictSorted = dict(sorted(thickFoilsDict.items(), key=lambda item: item[1]))
+
+    # Airfoil Indexes in Increasing Order of Thickness
+    global mediumFoilIndexSorted,thickFoilIndexSorted
+    mediumFoilIndexSorted = list(mediumFoilsDictSorted.keys())
+    thickFoilIndexSorted = list(thickFoilsDictSorted.keys())
+
+    # print("Medium Foils Sorted by Thickness: ", mediumFoilIndexSorted)
+    # print("Thick Foils Sorted by Thickness: ", thickFoilIndexSorted)
+
+    # Pre-Allocating Arrays
+    global x3,y3,x4,y4
+    x3 = [0]*len(mediumFoilIndexSorted)
+    y3= [0]*len(mediumFoilIndexSorted)
+    x4 = [0]*len(thickFoilIndexSorted)
+    y4= [0]*len(thickFoilIndexSorted)
+
+    # Sorting X and Y Arrays
+    i=0
+    for index in mediumFoilIndexSorted:
+        x3[i] = x[index]
+        y3[i] = y[index]
+        i+=1
+
+    i=0
+    for index in thickFoilIndexSorted:
+        x4[i] = x[index]
+        y4[i] = y[index]
+        i+=1
+    
 # User Input for Foil Transformation
-print("___________Falco Foil Transformation Animation___________ \n")
+def userFoilInput():
+    print("___________Falco Foil Transformation Animation___________ \n")
 
-print("Medium Foils")
-for i in mediumFoilIndexSorted:
-    print(str(mediumFoilIndexSorted.index(i))+ ": " + str(foilNames[i]))
+    print("Medium Foils")
+    for i in mediumFoilIndexSorted:
+        print(str(mediumFoilIndexSorted.index(i))+ ": " + str(foilNames[i]))
 
-print("Thick Foils")
-for i in thickFoilIndexSorted:
-    print(str(thickFoilIndexSorted.index(i))+ ": " + str(foilNames[i]))
+    print("Thick Foils")
+    for i in thickFoilIndexSorted:
+        print(str(thickFoilIndexSorted.index(i))+ ": " + str(foilNames[i]))
 
-foilCategory = input("Please enter the foil category you would like to transform (Medium or Thick): ")
+    foilCategory = input("Please enter the foil category you would like to transform (Medium or Thick): ")
 
 
-if foilCategory.lower() == "medium":
-    firstFoil = int(input("Please enter the corresponding index for your first foil: "))
-    secondFoil = int(input("Please enter the corresponding index for your second foil: "))
+    if foilCategory.lower() == "medium":
+        firstFoil = int(input("Please enter the corresponding index for your first foil: "))
+        secondFoil = int(input("Please enter the corresponding index for your second foil: "))
     
-    # Plotting Reference Plots
-    plt.plot(x3[firstFoil],y3[firstFoil])
-    plt.plot(x3[secondFoil],y3[secondFoil])
+        # Plotting Reference Plots
+        plt.plot(x3[firstFoil],y3[firstFoil])
+        plt.plot(x3[secondFoil],y3[secondFoil])
     
-    # Creating NP Arrays for Transformation
-    X1T = np.array(x3[firstFoil])
-    Y1T = np.array(y3[firstFoil])
-    X2T = np.array(x3[secondFoil])
-    Y2T = np.array(y3[secondFoil])
+        # Creating NP Arrays for Transformation
+        global X1T, Y1T, X2T, Y2T  
+        
+        X1T = np.array(x3[firstFoil])
+        Y1T = np.array(y3[firstFoil])
+        X2T = np.array(x3[secondFoil])
+        Y2T = np.array(y3[secondFoil])
+        
     
     
-elif foilCategory.lower() == "thick":
-    firstFoil = int(input("Please enter the corresponding index for your first foil: "))
-    secondFoil = int(input("Please enter the corresponding index for your second foil: "))
+    elif foilCategory.lower() == "thick":
+        firstFoil = int(input("Please enter the corresponding index for your first foil: "))
+        secondFoil = int(input("Please enter the corresponding index for your second foil: "))
     
-    # Plotting Reference Plots
-    plt.plot(x4[firstFoil],y4[firstFoil])
-    plt.plot(x4[secondFoil],y4[secondFoil])
+        # Plotting Reference Plots
+        plt.plot(x4[firstFoil],y4[firstFoil])
+        plt.plot(x4[secondFoil],y4[secondFoil])
     
-    # Creating NP Arrays for Transformation
-    X1T = np.array(x4[firstFoil])
-    Y1T = np.array(y4[firstFoil])
-    X2T = np.array(x4[secondFoil])
-    Y2T = np.array(y4[secondFoil])
+        # Creating NP Arrays for Transformation
+        X1T = np.array(x4[firstFoil])
+        Y1T = np.array(y4[firstFoil])
+        X2T = np.array(x4[secondFoil])
+        Y2T = np.array(y4[secondFoil])
+        
+    else:
+        print("Invalid foil category. Please enter either 'Medium' or 'Thick'.")
+
+
+def GUIFoilTransform(foilCategory,firstFoil,secondFoil):
+    foilData()
+    if foilCategory.lower() == "medium":
+        # Plotting Reference Plots
+        plt.plot(x3[firstFoil],y3[firstFoil])
+        plt.plot(x3[secondFoil],y3[secondFoil])
     
-else:
-    print("Invalid foil category. Please enter either 'Medium' or 'Thick'.")
+        # Creating NP Arrays for Transformation
+        global X1T, Y1T, X2T, Y2T  
+        
+        X1T = np.array(x3[firstFoil])
+        Y1T = np.array(y3[firstFoil])
+        X2T = np.array(x3[secondFoil])
+        Y2T = np.array(y3[secondFoil])
+        
+        return X1T,Y1T,X2T,Y2T
     
+    
+    elif foilCategory.lower() == "thick":
+        # Plotting Reference Plots
+        plt.plot(x4[firstFoil],y4[firstFoil])
+        plt.plot(x4[secondFoil],y4[secondFoil])
+    
+        # Creating NP Arrays for Transformation
+        X1T = np.array(x4[firstFoil])
+        Y1T = np.array(y4[firstFoil])
+        X2T = np.array(x4[secondFoil])
+        Y2T = np.array(y4[secondFoil])
+        
+        return X1T,Y1T,X2T,Y2T
+    
+    else:
+        pass 
+
+def foilTransformFigure():
+    # Airfoil Tranformation Animation Figure
+    global figure,foil
+    figure, axis = plt.subplots()
+    foil, = axis.plot([],[], lw=2, color='blue')
+    axis.set_xlim(-0.1, 1.1)
+    axis.set_ylim(-0.25, 0.25)
+
 def foilTransform(frame):
-    t = frame/100  # Linear Interpolation Factor of 1 as Frames = 100 
+    t = frame/100  
     
     x = (1 - t) * X1T + t * X2T
     y = (1 - t) * Y1T + t * Y2T
     
-    foil.set_data(x, y)
+    foil.set_data(x,y)
+    
     return (foil)
 
-ani = animation.FuncAnimation(fig=figure, func = foilTransform, frames=100, interval=10)
+if __name__ == "__main__":
+    # Main Query to Run Animation
+    mainQuery = input("Would you like to run the foil transformation animation without GUI? (yes/no): ")
 
-# Saves the Transformation Animation as a GIF
-writergif = animation.PillowWriter(fps=30)
-ani.save('Foil Transformation.gif',writer=writergif)
+    if mainQuery.lower() == "yes":
+        foilData()
+        foilTransformFigure()
+        userFoilInput()
 
-plt.show()
+        # Saves the Transformation Animation as a GIF
+        ani = animation.FuncAnimation(fig=figure, func = foilTransform, frames=100, interval=10)
+        writergif = animation.PillowWriter(fps=30)
+        ani.save('Foil Transformation.gif',writer=writergif)
+        plt.show()
+    
+    elif mainQuery.lower() == "no":
+        print("Use GUI to run the foil transformation animation!")
+    
+    else:
+        print("Invalid input. Please enter 'yes' or 'no'.")
